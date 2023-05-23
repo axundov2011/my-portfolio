47b9624d15691685674923c149a7b9aa32923dc9
aykhan.akhundov <aykhan.akhundov@a2z.az>
2023-05-23 15:38:20 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import About from "./components/About"
+import Contact from './components/Contact'
+import Experience from "./components/Experience"
+import Hero from "./components/Hero"
+import Tech from "./components/Tech"
+import Works from "./components/Works"
+import StarsCanvas from "./components/canvas/Stars"
+// import {Navbar, About, Contact, Experience, Hero, Tech, Works, StarsCanvas} from './components'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
---
+++ src/components/canvas/index.js
@@ -0,0 +1,7 @@
+import ComputersCanvas from "./Computers";
+import BallCanvas from "./Ball";
+import StarsCanvas from "./Stars";
+import EarthCanvas from "./Earth";
+
+
+export {ComputersCanvas, BallCanvas, StarsCanvas, EarthCanvas};
